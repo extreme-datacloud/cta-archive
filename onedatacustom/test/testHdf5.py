72d1bfc1fd9a6774175c1f1d96ec7ed2f6d1cf39
frederic Gillardo <gillardo@lapp.in2p3.fr>
2018-10-15 16:52:42 +0200

    add test

--- onedatacustom/test/testHdf5.py
+++ onedatacustom/test/testHdf5.py
@@ -1,8 +1,10 @@
 import unittest
-
-def fun(x):
-    return x + 1
-
+import os
+import onedatacustom.metadataextractor
 class Hdf5Test(unittest.TestCase):
-    def test(self):
-        self.assertEqual(fun(3), 4)
+    currentDir=os.path.dirname(os.path.abspath(__file__))
+    def testHDF5(self):
+        self.assertEqual(onedatacustom.metadataextractor.extract(self.currentDir+"/ressources/gamma_test.hdf5"), '{"TelescopeID": "AFX123", "trigger": 112456, "CaptureDate": "2012-04-23T18:25:08", "EventID": "UIDASDBN456"}')
+
+    def testZfits(self):
+        self.assertEqual(onedatacustom.metadataextractor.extract(self.currentDir+"/ressources/example_9evts_NectarCAM.fits.fz"), '{"TelescopeID": "0", "CaptureDate": "1975-07-08T09:35:45"}')
