4c38ecedbb6a497d67d5c3ce3b2e7742b2131cf6
frederic Gillardo <gillardo@lapp.in2p3.fr>
2019-02-11 16:34:36 +0100

    restQuery

--- onedatacustom/metadatagenerator.py
+++ onedatacustom/metadatagenerator.py
@@ -7,6 +7,7 @@ from datetime import datetime
 import time
 import sys
 import shutil
+import onedatacustom.restquery
 
 
 
@@ -27,14 +28,20 @@ class MetaDataGeneratorHdf5:
     current_time= 'currenttime'
 
     def __init__(self, h5_file_path):
-        self.h5_file = h5py.File(h5_file_path, 'w')
+        self.h5_file = h5py.File(h5_file_path, 'a')
         self.meta_dataset=self.h5_file
 
     def set_trigger_value(self, trigger_value):
         self.meta_dataset.attrs[self.trigger]=trigger_value
 
+    def get_trigger_value(self):
+        return self.meta_dataset.attrs[self.trigger]
+
     def set_telescope_id_value(self, telescope_ID_value):
         self.meta_dataset.attrs[self.telescope_ID]=telescope_ID_value
+
+    def get_telescope_id_value(self):
+        return self.meta_dataset.attrs[self.telescope_ID]
 
     def set_capture_date_value(self, time_stamp_value):
         self.meta_dataset.attrs[self.capture_date]=time_stamp_value
@@ -42,42 +49,51 @@ class MetaDataGeneratorHdf5:
         # datetime.strptime("2012-04-23T16:25:43.511Z","%Y-%m-%dT%H:%M:%S.%fZ").time()
         #self.h5_file.create_dataset(self.capture_date, (1,1), 'i8',time_stamp_value)
 
+    def get_telescope_id_value(self):
+        return self.meta_dataset.attrs[self.capture_date]
+
     def set_event_id_value(self, event_ID_value):
         self.meta_dataset.attrs[self.event_id]=event_ID_value
         #ascii_event_value = [event_ID_value.encode("ascii", "ignore") ]
         #self.h5_file.create_dataset(self.event_id, (1,1), 'S20', ascii_event_value)
 
-    def generate_several_HDF5_file(nbr_of_file_per_directory, scalefactor, path_to_volumes, sleeptime=1):
+    def get_event_id_value(self):
+        return self.meta_dataset.attrs[self.event_id]
+
+    def generate_several_HDF5_file(nbr_of_file_per_directory, scalefactor, root_path_to_volumes, relative_path_to_volume, sleeptime=1, connectiontuple=None):
         try:
-            shutil.rmtree(path_to_volumes)
+            shutil.rmtree(root_path_to_volumes+relative_path_to_volume)
         except :
             print ("Oops! directory does not yet exist")
 
-        os.mkdir(path_to_volumes)
+        os.mkdir(root_path_to_volumes+relative_path_to_volume)
         start= time.time()
         for j in range (1,scalefactor+1):
-            pathdirectory1=path_to_volumes+"/"+str(j)
-            os.mkdir(pathdirectory1)
+            pathdirectory1=relative_path_to_volume+"/"+str(j)
+            os.mkdir(root_path_to_volumes+pathdirectory1)
             for k in range (1,scalefactor+1):
                 pathdirectory2=pathdirectory1+"/"+str(k)
-                os.mkdir(pathdirectory2)
+                os.mkdir(root_path_to_volumes+pathdirectory2)
                 print (str(j)+":"+str(k))
                 for i in range (0,nbr_of_file_per_directory):
                     file_id=int(str(j)+str(k)+str(i).zfill(3))
-                    metadatagenerator=generate(pathdirectory2+"/gamma_test_generated_"+str(file_id)+".hdf5")
+                    fileName=pathdirectory2+"/gamma_test_generated_"+str(file_id)+".hdf5"
+                    metadatagenerator=generate(root_path_to_volumes+fileName)
                     metadatagenerator.set_trigger_value(file_id)
                     metadatagenerator.set_capture_date_value(1335198308+file_id)
                     metadatagenerator.set_event_id_value("UIDASDBN"+str(file_id/10))
                     metadatagenerator.set_telescope_id_value("AFX"+str(file_id%100))
+                    if connectiontuple!=None:
+                        restquery=onedatacustom.restquery.RestQuery(connectiontuple[0],connectiontuple[1])
+                        restquery.set_attribute(fileName,root_path_to_volumes)
+
+
                 time.sleep(sleeptime)
         end=time.time()
         print ("time to generate {:d} files is {:f} s".format(nbr_of_file_per_directory*(scalefactor-1)**2, end-start))
 
 def generate(file_path):
-    if file_path.endswith('.fz'):
-        print("not supported")
-        # return MetaDataExtractorZfits(file_path).to_json()
-    elif file_path.endswith('.hdf5'):
+    if file_path.endswith('.hdf5'):
         return MetaDataGeneratorHdf5(file_path)
     else:
         raise Exception("File format not supported")
@@ -88,11 +104,17 @@ def main():
     print (sys.argv[2])
 
     if len(sys.argv)>4 :
-        path_to_volumes=sys.argv[4]
+        root_path_to_volumes=sys.argv[4]
     else :
-        path_to_volumes=os.path.dirname(os.path.abspath(__file__))+"/ressources/volumes/"
+        root_path_to_volumes=os.path.dirname(os.path.abspath(__file__))+"/ressources/volumes/"
 
-    MetaDataGeneratorHdf5.generate_several_HDF5_file(int(sys.argv[1]),int(sys.argv[2]),path_to_volumes,int(sys.argv[3]))
+    if len(sys.argv)>5 :
+        relative_path_to_volumes=sys.argv[5]
+    else :
+        relative_path_to_volumes="/space/"
+
+
+    MetaDataGeneratorHdf5.generate_several_HDF5_file(int(sys.argv[1]),int(sys.argv[2]),root_path_to_volumes,relative_path_to_volumes,int(sys.argv[3]),("lapp-xdc01.in2p3.fr","MDAxNWxvY2F00aW9uIG9uZXpvbmUKMDAzMGlkZW500aWZpZXIgOTIzNDBmZjkyYTI1Y2RjZTlhM2ZmMWIyYTE5MGJjMGEKMDAxYWNpZCB00aW1lIDwgMTU3NTM4MDY00MwowMDJmc2lnbmF00dXJlIGnMtasbKyvaMI84gZo0061QqELeHb1KJBFJulqOmTdBsCg"))
 
 if __name__ == '__main__':
     main()
--- onedatacustom/restquery.py
+++ onedatacustom/restquery.py
@@ -1,13 +1,13 @@
 import requests
 import json
 import csv
-import os
-
+import onedatacustom.metadatagenerator as mg
+import onedatacustom.metadataextractor as me
 
 class RestQuery:
 
     def __init__(self, hostname,token):
-        self. headers = {'X-Auth-Token': token}
+        self. headers = {'X-Auth-Token': token,"Content-type": "application/json"}
         self.hostname=hostname
         requests.packages.urllib3.disable_warnings()
 
@@ -16,23 +16,34 @@ class RestQuery:
         response = requests.request("GET", url, headers=self.headers, verify=False)
         return (response.text)
 
-    def get_attributefromfile(self,path):
-        url = "https://"+self.hostname+"/api/v3/oneprovider/attributes/SPACE-SMALL/"+path
+    def get_attributefromfile(self, fileName, space_name):
+        url = "https://" + self.hostname +"/api/v3/oneprovider/attributes/" + space_name +"/" + fileName
         response = requests.request("GET", url, headers=self.headers, verify=False)
         #print(url)
         #print(response.text)
         return (json.loads(response.text))
 
-    def get_attributeextendedfromfile(self,path):
-        url = "https://"+self.hostname+"/api/v3/oneprovider/attributes/SPACE-SMALL/"+path
-        querystring= {"extended":"true"}
+    def get_attributeextendedfromfile(self, fileName, space_name):
+        url = "https://" + self.hostname +"/api/v3/oneprovider/attributes/" + space_name +"/" + fileName+"?extended=true"
+        querystring= {"%22extended%22":"%22true%22"}
         response = requests.request("GET", url, headers=self.headers, verify=False, params=querystring)
         #print(url)
         #print(response.text)
         return (json.loads(response.text))
 
+    def set_attribute(self, fileName, root_path_to_volumes):
+        meta_data_extractor=me.MetaDataExtractorHdf5(root_path_to_volumes+fileName)
+        url = "https://" + self.hostname +"/api/v3/oneprovider/metadata/" + fileName
+        response = requests.request( "PUT", url, json=meta_data_extractor.to_json(), headers=self.headers, verify=False)
 
-def main(pathdirectory0, scalefactor, nbr_of_file_per_directory):
+        print(url)
+        print(response.text)
+        #self.pretty_print_POST(response)
+
+
+
+
+def retrieveMetadata(pathdirectory0, scalefactor, nbr_of_file_per_directory):
 
     restquery=RestQuery("cc-xdc02.in2p3.fr","MDAxNWxvY2F00aW9uIG9uZXpvbmUKMDAzMGlkZW500aWZpZXIgOTIzNDBmZjkyYTI1Y2RjZTlhM2ZmMWIyYTE5MGJjMGEKMDAxYWNpZCB00aW1lIDwgMTU3NTM4MDY00MwowMDJmc2lnbmF00dXJlIGnMtasbKyvaMI84gZo0061QqELeHb1KJBFJulqOmTdBsCg")
 
@@ -51,10 +62,12 @@ def main(pathdirectory0, scalefactor, nbr_of_file_per_directory):
                     file_id=int(str(j)+str(k)+str(i).zfill(3))
                     filename=pathdirectory2+"/gamma_test_generated_"+str(file_id)+".hdf5"
                     print (filename)
-                    jsonAttribute=restquery.get_attributefromfile(filename)
-                    jsonAttributeExtended=restquery.get_attributeextendedfromfile(filename)
+                    jsonAttribute=restquery.get_attributefromfile(filename,"SPACE_SMALL")
+                    jsonAttributeExtended=restquery.get_attributeextendedfromfile(filename, "SPACE_SMALL")
                     writer.writerow({'file':jsonAttribute["name"],'ctime':jsonAttribute["ctime"],'ctime':jsonAttribute["ctime"], 'atime':jsonAttribute["atime"],'Extractiontime':int(jsonAttributeExtended["onedata_json"]["Extractiontime"])})
 
 
 if __name__ == '__main__':
-    main("testA",2 ,500)
+   restquery=RestQuery("lapp-xdc01.in2p3.fr","MDAxNWxvY2F00aW9uIG9uZXpvbmUKMDAzMGlkZW500aWZpZXIgOTIzNDBmZjkyYTI1Y2RjZTlhM2ZmMWIyYTE5MGJjMGEKMDAxYWNpZCB00aW1lIDwgMTU3NTM4MDY00MwowMDJmc2lnbmF00dXJlIGnMtasbKyvaMI84gZo0061QqELeHb1KJBFJulqOmTdBsCg")
+   restquery.set_attribute("testA4/1/1/gamma_test_generated_11001.hdf5","SPACE-SMALL")
+
